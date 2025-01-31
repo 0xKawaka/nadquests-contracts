76adfbff22cd6b6a700198804289022acf16c584
0xKawaka <0xKawaka@proton.me>
2025-01-31 13:26:28 +0100

    badges

--- script/Badges.s.sol
+++ script/Badges.s.sol
@@ -6,12 +6,13 @@ import {Badges} from "../src/Badges.sol";
 
 contract DeployBadges is Script {
     address trustedAuthority = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    address owner = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
     function run() external {
         vm.startBroadcast();
 
         Badges badges = new Badges();
-        badges.initialize(trustedAuthority, trustedAuthority);
+        badges.initialize(trustedAuthority, owner);
 
         vm.stopBroadcast();
     }
